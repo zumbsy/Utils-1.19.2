d49ef55c9bfe11fd89121d34c55e78f492266834
zumbsy <dominikpopp27@gmail.com>
2022-09-16 16:46:28 +0200

    added important comment

---
+++ src/main/java/net/zumbsy/zmbutilities/ZmbUtilitiesClient.java
@@ -0,0 +1,10 @@
+package net.zumbsy.zmbutilities;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZmbUtilitiesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
